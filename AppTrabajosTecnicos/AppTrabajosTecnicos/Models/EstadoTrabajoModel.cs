46dd5730f074b18747ad24e886b05bae8b9f0ec7
santiflo <santiflo@javerianacali.edu.co>
2020-04-28 22:09:49 -0500

    AppTrabajosTecnicos
    Hasta el momento solo se ha planteado todo el modelo, es necesario agregar que atributos van a ser enviados y recibidos por el Json

---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Models/EstadoTrabajoModel.cs
@@ -0,0 +1,28 @@
+﻿using AppTrabajosTecnicos.Servicios.Propagacion;
+
+namespace AppTrabajosTecnicos.Models
+{
+	internal class EstadoTrabajoModel: NotificationObject
+	{
+		#region Atributos
+		public string ID { get; set; }
+		public string estado { get; set; } 
+		private TrabajoModel trabajo;
+		#endregion Atributos
+
+		#region Constructores
+		#endregion Constructores
+
+		#region Getter/Setter
+		public TrabajoModel Trabajo
+		{
+			get { return trabajo; }
+			set
+			{
+				trabajo = value;
+				OnPropertyChanged();
+			}
+		}
+		#endregion Getter/Setter
+	}
+}
---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Models/UsuarioModel.cs
@@ -0,0 +1,87 @@
+﻿using AppTrabajosTecnicos.Servicios.Propagacion;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTrabajosTecnicos.Models
+{
+	class UsuarioModel : NotificationObject
+	{
+		#region Atributos
+		[JsonIgnore]
+		public long ID { get; set; }
+		[JsonProperty("idUsuario")]
+		private long idUsuario { get; set; }
+		[JsonProperty("emailUsuario")]
+		public String Email { get; set; }
+		[JsonProperty("nombreUsuario")]
+		private String nombre;
+		private List<TelefonoModel> telefonos;
+		private List<DireccionModel> direcciones;
+		private PerfilModel perfil;
+		private List<TrabajoModel> trabajos;
+
+		#endregion Atributos
+
+
+		#region Constructores
+		public UsuarioModel(PerfilModel perfil)
+		{
+			this.perfil = perfil;
+		}
+		#endregion Constructores
+
+		#region Getter/Setter
+		public String Nombre
+		{
+			get { return nombre; }
+			set
+			{
+				nombre = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public PerfilModel Perfil
+		{
+			get { return perfil; }
+			set
+			{
+				perfil = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public List<DireccionModel> Direcciones
+		{
+			get { return direcciones; }
+			set
+			{
+				direcciones = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public List<TelefonoModel> Telefonos
+		{
+			get { return telefonos; }
+			set
+			{
+				telefonos = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public List<TrabajoModel> Trabajos
+		{
+			get { return trabajos; }
+			set
+			{
+				trabajos = value;
+				OnPropertyChanged();
+			}
+		}
+		#endregion Getter/Setter
+	}
+}
