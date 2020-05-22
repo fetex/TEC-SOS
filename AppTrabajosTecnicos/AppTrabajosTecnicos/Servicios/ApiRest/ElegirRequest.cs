7202241e59b7b654680f44eceb9c89f0fe5c3183
santiflo <santiflo@javerianacali.edu.co>
2020-05-21 21:41:15 -0500

    Api Rest

---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/ElegirRequest.cs
@@ -0,0 +1,43 @@
+﻿using AppTrabajosTecnicos.Configuracion;
+using AppTrabajosTecnicos.Models.ModelsAux;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTrabajosTecnicos.Servicios.APIRest
+{
+    public class ElegirRequest<T>
+    {
+        #region Properties
+        public Request<T> EstrategiaEnvio { get; set; }
+        public ConfiguracionRest ConfiguracionRest { get; set; }      
+        #endregion Properties
+
+        #region Initialize
+        public ElegirRequest() 
+        {
+            ConfiguracionRest = new ConfiguracionRest();
+        }
+        #endregion Initialize
+
+        #region Metodos 
+        public void ElegirEstrategia(string verbo, string url)
+        {
+            var diccionario = ConfiguracionRest.VerbosConfiguracion;
+            string nombreClase;
+            if (diccionario.TryGetValue(verbo.ToUpper(), out nombreClase))  //Verb Upper
+            {
+                Type tipoClase = Type.GetType(nombreClase);
+                EstrategiaEnvio = (Request<T>)Activator.CreateInstance(tipoClase);
+            }
+        }
+
+        public async Task<APIResponse> EjecutarEstrategia(T objecto)
+        {
+            var response = await EstrategiaEnvio.SendRequest(objecto);
+            return response;
+        }
+        #endregion Metodos
+    }
+}
---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/RequestParametros.cs
@@ -0,0 +1,71 @@
+﻿using AppTrabajosTecnicos.Servicios.APIRest;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTrabajosTecnicos.Models.ModelsAux
+{
+    /* GET AND DELETE */
+    public class RequestParametros<T> : Request<T>
+    {
+        public RequestParametros(string url, string verbo)
+        {
+            Url = url;
+            Verbo = verbo;
+        }
+
+        #region Metodos
+        public override async Task<APIResponse> SendRequest(T objecto)
+        {
+            APIResponse respuesta = new APIResponse()
+            {
+                Code = 400,
+                IsSucess = false,
+                Response = ""
+            };
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var verboHttp = (Verbo == "GET") ? HttpMethod.Get : HttpMethod.Delete;
+                    await this.ConstruirUrl(objecto);
+                    HttpRequestMessage requestMessage = new HttpRequestMessage(verboHttp, Url);
+                    requestMessage = ServicioHeaders.AgregarCabeceras(requestMessage);
+                    HttpResponseMessage HttpResponse = await client.SendAsync(requestMessage);
+                    respuesta.Code = Convert.ToInt32(HttpResponse.StatusCode);
+                    respuesta.IsSucess = HttpResponse.IsSuccessStatusCode;
+                    respuesta.Response = await HttpResponse.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch(Exception)
+            {
+                respuesta.Response = "Error al momento de llamar al servidor";
+            }
+
+            return respuesta;
+        }
+
+        private async Task ConstruirUrl(T parametros)
+        {
+            ParametersRequest Parametros = parametros as ParametersRequest;
+
+            if (Parametros.Parametros.Count > 0)
+            {
+                Url = (Url.Substring(Url.Length - 1) == "/") ? Url.Remove(Url.Length - 1) : Url;
+                Parametros.Parametros.ForEach(p => Url += "/" + p); 
+            }
+
+            if (Parametros.QueryParametros.Count > 0)
+            {
+                var queryParameters = await new FormUrlEncodedContent(Parametros.QueryParametros).ReadAsStringAsync();
+                Url += queryParameters;
+            }
+        }
+
+        #endregion Metodos
+    }
+}
