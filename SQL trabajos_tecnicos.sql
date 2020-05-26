3d740b0528210d076c823af1d89f5e81ba10d437
Dicson <fetex98@gmail.com>
2020-05-26 00:09:44 -0500

    update field of database

--- SQL trabajos_tecnicos.sql
+++ SQL trabajos_tecnicos.sql
@@ -1,5 +1,40 @@
-CREATE DATABASE IF NOT EXISTS trabajos_tecnicos;
-USE trabajos_tecnicos;
+CREATE DATABASE IF NOT EXISTS TECSOS;
+USE TECSOS;
+
+#Tabla Categoria
+CREATE TABLE IF NOT EXISTS categoria(
+ID_Categoria int NOT NULL AUTO_INCREMENT,
+categoria varchar(50) NOT NULL,
+CONSTRAINT PK_Categoria PRIMARY KEY (ID_Categoria));
+
+#Tabla Estado servicio
+CREATE TABLE IF NOT EXISTS estadoservicio(
+ID_EstadoServicio int NOT NULL AUTO_INCREMENT,
+estado varchar(50) NOT NULL,
+CONSTRAINT PK_EstadoServicio PRIMARY KEY (ID_EstadoServicio));
+
+#Tabla Usuario
+CREATE TABLE IF NOT EXISTS usuario(
+ID_Usuario int NOT NULL AUTO_INCREMENT,
+nombre varchar(50) NOT NULL,
+email varchar(50) NOT NULL UNIQUE,
+CONSTRAINT PK_usuario PRIMARY KEY (ID_Usuario));
+
+#Tabla Telefono
+CREATE TABLE IF NOT EXISTS telefono(
+ID_Telefono int NOT NULL AUTO_INCREMENT,
+tipo varchar(25) NOT NULL,
+numero varchar(15) NOT NULL,
+ID_Usuario int,
+CONSTRAINT PK_Telefono PRIMARY KEY (ID_Telefono),
+FOREIGN KEY (ID_Usuario) REFERENCES usuario(ID_Usuario));
+
+#Tabla cliente
+CREATE TABLE IF NOT EXISTS cliente(
+ID_Cliente int NOT NULL AUTO_INCREMENT,
+ID_Usuario int,
+CONSTRAINT PK_cliente PRIMARY KEY (ID_Cliente),
+FOREIGN KEY (ID_Usuario) REFERENCES usuario(ID_Usuario));
 
 #Tabla Direccion
 CREATE TABLE IF NOT EXISTS direccion(
@@ -7,67 +42,33 @@ direccion_id int NOT NULL AUTO_INCREMENT,
 direccion varchar(50) NOT NULL,
 complemento varchar(50),
 indicacion tinytext,
-CONSTRAINT PK_Direccion PRIMARY KEY (direccion_id));
-
-#Tabla Categoria
-CREATE TABLE IF NOT EXISTS categoria(
-categoria_id int NOT NULL AUTO_INCREMENT,
-categoria varchar(50) NOT NULL,
-CONSTRAINT PK_Categoria PRIMARY KEY (categoria_id));
-
-#Tabla Estado servicio
-CREATE TABLE IF NOT EXISTS estadoservicio(
-estadoServicio_id int NOT NULL AUTO_INCREMENT,
-estado varchar(50) NOT NULL,
-CONSTRAINT PK_EstadoServicio PRIMARY KEY (estadoServicio_id));
-
-#Tabla Usuario
-CREATE TABLE IF NOT EXISTS usuario(
-usuario_id int NOT NULL AUTO_INCREMENT,
-nombre varchar(50) NOT NULL,
-email varchar(50) NOT NULL UNIQUE,
-CONSTRAINT PK_usuario PRIMARY KEY (usuario_id));
-
-#Tabla Telefono
-CREATE TABLE IF NOT EXISTS telefono(
-telefono_id int NOT NULL AUTO_INCREMENT,
-tipo varchar(25) NOT NULL,
-numero varchar(15) NOT NULL,
-usuario_id4 int,
-CONSTRAINT PK_Telefono PRIMARY KEY (telefono_id),
-FOREIGN KEY (usuario_id4) REFERENCES usuario(usuario_id));
-
-#Tabla cliente
-CREATE TABLE IF NOT EXISTS cliente(
-cliente_id int NOT NULL AUTO_INCREMENT,
-direccion_id1 int,
-usuario_id1 int,
-CONSTRAINT PK_cliente PRIMARY KEY (cliente_id),
-FOREIGN KEY (direccion_id1) REFERENCES direccion(direccion_id),
-FOREIGN KEY (usuario_id1) REFERENCES usuario(usuario_id));
+ID_Usuario int ,
+CONSTRAINT PK_Direccion PRIMARY KEY (direccion_id),
+FOREIGN KEY (ID_Usuario) REFERENCES usuario(ID_Usuario));
 
 #Tabla tecnico
 CREATE TABLE IF NOT EXISTS tecnico(
-tecnico_id int NOT NULL AUTO_INCREMENT,
+ID_Tecnico int NOT NULL AUTO_INCREMENT,
 calificacion float,
-usuario_id2 int,
-categoria_id1 int,
-CONSTRAINT PK_tecnico PRIMARY KEY (tecnico_id),
-FOREIGN KEY (usuario_id2) REFERENCES usuario(usuario_id),
-FOREIGN KEY (categoria_id1) REFERENCES categoria(categoria_id));
+ID_Usuario int,
+ID_Categoria int,
+CONSTRAINT PK_tecnico PRIMARY KEY (ID_Tecnico),
+FOREIGN KEY (ID_Usuario) REFERENCES usuario(ID_Usuario),
+FOREIGN KEY (ID_Categoria) REFERENCES categoria(ID_Categoria));
 
 #Tabla servicios
 CREATE TABLE IF NOT EXISTS servicio(
 servicio_id int NOT NULL AUTO_INCREMENT,
 descripcion tinytext NOT NULL,
 fecha datetime,
-estadoServicio_id1 int,
-cliente_id1 int,
-tecnico_id1 int,
+ID_EstadoServicio int,
+ID_Cliente int,
+ID_Tecnico int,
 CONSTRAINT PK_trabajo PRIMARY KEY (servicio_id),
-FOREIGN KEY (estadoServicio_id1) REFERENCES estadoservicio(estadoServicio_id),
-FOREIGN KEY (cliente_id1) REFERENCES cliente(cliente_id),
-FOREIGN KEY (tecnico_id1) REFERENCES tecnico(tecnico_id));
+FOREIGN KEY (ID_EstadoServicio) REFERENCES estadoservicio(ID_EstadoServicio),
+FOREIGN KEY (ID_Cliente) REFERENCES cliente(ID_Cliente),
+FOREIGN KEY (ID_Tecnico) REFERENCES tecnico(ID_Tecnico));
+
 
 /*
 CREATE TABLE IF NOT EXISTS mensajes(
