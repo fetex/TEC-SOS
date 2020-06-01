3a806303cbeef2898b8a4e72c852ea818cb2a7bc
Dicson <fetex98@gmail.com>
2020-06-01 13:35:33 -0500

    ImagesResource

---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/VisualComponents/ImagenResourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTrabajosTecnicos.VisualComponents
+{
+    [ContentProperty(nameof(Source))]
+    public class ImagenResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source, typeof(ImagenResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
