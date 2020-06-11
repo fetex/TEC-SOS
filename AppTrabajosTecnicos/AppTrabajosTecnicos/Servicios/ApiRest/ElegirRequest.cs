5341bab4babe76374647e54864f3ba2d8147fbdd
Dicson <fetex98@gmail.com>
2020-06-11 16:20:56 -0500

    prueba

--- AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/ElegirRequest.cs
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/ElegirRequest.cs
@@ -11,37 +11,33 @@ namespace AppTrabajosTecnicos.Servicios.APIRest
     {
         #region Properties
         public Request<T> EstrategiaEnvio { get; set; }
-        public ConfiguracionRest ConfiguracionRest { get; set; }
+        public ConfiguracionRest ConfiguracionRest { get; set; }      
         #endregion Properties
 
         #region Initialize
-        public ElegirRequest()
+        public ElegirRequest() 
         {
             ConfiguracionRest = new ConfiguracionRest();
         }
         #endregion Initialize
 
-        #region Métodos
+        #region Metodos 
         public void ElegirEstrategia(string verbo, string url)
         {
             var diccionario = ConfiguracionRest.VerbosConfiguracion;
             string nombreClase;
-            if (diccionario.TryGetValue(verbo.ToUpper(), out nombreClase))
+            if (diccionario.TryGetValue(verbo.ToUpper(), out nombreClase))  //Verb Upper
             {
                 Type tipoClase = Type.GetType(nombreClase);
-                Type[] typeArgs = { typeof(T) };
-                var genericClass = tipoClase.MakeGenericType(typeArgs);
-                EstrategiaEnvio = (Request<T>)Activator.CreateInstance(genericClass, url, verbo.ToUpper());
+                EstrategiaEnvio = (Request<T>)Activator.CreateInstance(tipoClase);
             }
         }
 
-        public async Task<APIResponse> EjecutarEstrategia(T objecto, ParametersRequest parametersRequest = null)
+        public async Task<APIResponse> EjecutarEstrategia(T objecto)
         {
-            parametersRequest = parametersRequest ?? new ParametersRequest();
-            await EstrategiaEnvio.ConstruirURL(parametersRequest);
             var response = await EstrategiaEnvio.SendRequest(objecto);
             return response;
         }
-        #endregion Métodos
+        #endregion Metodos
     }
 }
--- AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/Request.cs
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Servicios/ApiRest/Request.cs
@@ -1,7 +1,6 @@
 ﻿using AppTrabajosTecnicos.Models.ModelsAux;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +8,10 @@ namespace AppTrabajosTecnicos.Servicios.APIRest
 {
     public abstract class Request<T>
     {
-
+ 
         #region Properties
         protected string Url { get; set; }
         protected string Verbo { get; set; }
-        protected string UrlParameters { get; set; }
-
         private static ServicioHeaders servicioHeaders;
 
         #endregion Properties
@@ -28,34 +25,14 @@ namespace AppTrabajosTecnicos.Servicios.APIRest
                 {
                     servicioHeaders = new ServicioHeaders();
                 }
+
                 return servicioHeaders;
             }
         }
+    
         #endregion Getters / Setters
-
-        #region Métodos
+        #region Metodos 
         public abstract Task<APIResponse> SendRequest(T objecto);
-
-        public async Task ConstruirURL(ParametersRequest parametros)
-        {
-            ParametersRequest Parametros = parametros as ParametersRequest;
-            string newURL = Url;
-
-            if (Parametros.Parametros.Count > 0)
-            {
-                newURL = (newURL.Substring(Url.Length - 1) == "/") ? newURL.Remove(newURL.Length - 1) : newURL;
-                Parametros.Parametros.ForEach(p => newURL += "/" + p);
-            }
-
-            if (Parametros.QueryParametros.Count > 0)
-            {
-                var queryParameters = await new FormUrlEncodedContent(Parametros.QueryParametros).ReadAsStringAsync();
-                newURL += queryParameters;
-            }
-
-            UrlParameters = newURL;
-        }
-
-        #endregion Métodos
+        #endregion Metodos
     }
 }
