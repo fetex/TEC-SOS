81a9692706cd7a5a1185bd8c79f02cafbdbf1e8c
santiflo <santiflo@javerianacali.edu.co>
2020-06-12 03:55:04 -0500

    Login View

---
+++ AppTrabajosTecnicos/AppTrabajosTecnicos/Views/CrearCuentaView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTrabajosTecnicos.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CrearCuentaView : ContentPage
+	{
+		public CrearCuentaView()
+		{
+			InitializeComponent();
+		}
+	}
+}
